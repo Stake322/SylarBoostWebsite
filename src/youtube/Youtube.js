a651375b99fa8be9f11d7690aded03f8e4fd480b
Stake322 <81745125+Stake322@users.noreply.github.com>
2021-11-23 20:58:29 +0400

    add youtubecontent

--- src/youtube/Youtube.js
+++ src/youtube/Youtube.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Container, Menu, Segment, Image, Sticky, Grid } from 'semantic-ui-react'
+import { Container, Menu, Segment, Image, Sticky, Grid, Header, Embed } from 'semantic-ui-react'
 import rub from "../img/youtubeIMG/1.png";
 import voker from "../img/youtubeIMG/6.png";
 import slark from "../img/youtubeIMG/4.png";
 import luna from "../img/youtubeIMG/7.png";
+import pudge from "../img/youtubeIMG/pudge.jpg";
 
 
 
@@ -47,7 +48,7 @@ const Youtube = () => {
     }
     const frBlock = {
         backgroundColor: "white",
-        height: "600px",
+        height: "700px",
         opacity: '100%',
         position: 'sticky',
         // bottom: "0px",
@@ -61,17 +62,14 @@ const Youtube = () => {
                         <Grid>
                             <Grid.Column width={12}>
                                 <p style={{ fontSize: "130%" }}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-                                    numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-                                    optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-                                    obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-                                    nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-                                    tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-                                    quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-                                    sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-                                    recusandae alias error harum maxime adipisci amet laborum. Perspiciatis
-                                    minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit.
+                                    <Header>Youtube канал:</Header>
+                                    Привет БРОУ! Именно с этой фразы начинается каждое видео.
+                                    Ютуб канал посвящен игре DOTA 2. Думаю по страничке сайта становится понятно.
+                                    Канал создавался как определенный тест моих навыков, потому что заниматься созданием контента это огромная работа.
+                                    Канал называется Mamau dota  в честь моей фамилии Мамаев и дота, тут нет ничего сложного....
+                                    Изначальной идеей ютуба было просто создание видео-гайдов, руководства для того чтобы игроки начали правильно играть.
+                                    Максимальный рейтинг у меня был 7300 ММР (match making raiting),а место занимал в  Европе -  1000.
+                                    Это очень хороший показатель понимания моей игры.
                                 </p>
                             </Grid.Column>
                             <Grid.Column width={4}>
@@ -90,17 +88,12 @@ const Youtube = () => {
                             </Grid.Column>
                             <Grid.Column width={12}>
                                 <p style={{ fontSize: "130%" }}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-                                    numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-                                    optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-                                    obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-                                    nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-                                    tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-                                    quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-                                    sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-                                    recusandae alias error harum maxime adipisci amet laborum. Perspiciatis
-                                    minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit.
+                                    <Header>КОУЧИНГ:</Header>
+                                    Целый год занимаясь созданием контента познакомился со многими людьми, а также помогал ребятам поднимать свой скилл в виде индивидуальных тренировок.
+                                    Кто не понимает о чем идёт речь, это репетиторство в доте.
+                                    Занятия по итогу стали оцень эффективны, огромное количество довольных клиентов и положительных отзывов.
+                                    За год провёл более 250 часов индвидуальных тренировок.
+                                    Ознакомится с отзывами можно здесь:
                                 </p>
                             </Grid.Column>
                         </Grid>
@@ -113,17 +106,13 @@ const Youtube = () => {
                         <Grid>
                             <Grid.Column width={12}>
                                 <p style={{ fontSize: "130%" }}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-                                    numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-                                    optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-                                    obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-                                    nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-                                    tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-                                    quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-                                    sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-                                    recusandae alias error harum maxime adipisci amet laborum. Perspiciatis
-                                    minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit.
+                                    <Header>КУРС:</Header>
+                                    Безусловно, можно перегореть, если постоянно заниматься с людьми - помогать им решить их проблемы.
+                                    Поэтому я решил сделать свой курс, где четко и ясно изложил всю ту информацию, которую скопил за свой 8 летний стаж игры.
+                                    Также я активно выступал на турнирах и занимал призовые места со своей командой. В команде был капитаном, разбирал игровые моменты и драфтил.
+                                    В курсе собранны весь мой опыт за киберспортивную и тренерскую деятельность.На данный момент курс состоит из 30 видеороликов, выдающиеся пожизненно.
+                                    Он очень удобен, так как можно сначала ознакомится с теорией, а потом поиграть и улучшить свои практические навыки.
+                                    На индивидуальных занятиях сложно рассмотреть всё стороны игровых механик и моментов, а в курсе рассмотрен геймплей, задачи и прочее для каждой роли.
                                 </p>
                             </Grid.Column>
                             <Grid.Column width={4}>
@@ -143,18 +132,15 @@ const Youtube = () => {
                         </Grid.Column>
                         <Grid.Column width={12}>
                             <p style={{ fontSize: "130%" }}>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                                molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-                                numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-                                optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-                                obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-                                nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-                                tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-                                quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-                                sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-                                recusandae alias error harum maxime adipisci amet laborum. Perspiciatis
-                                minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit.
+                                <Header>ИТОГ:</Header>
+                                Эта страничка создана для практики в дизайных навыках, надеюсь он понравился Вам и ознакомления с одной из моих деятельностей.
+                                Также можете посмотреть ознакомительный видеоролик, что нового можно узнать в курсе. Приятного просмотра!
                             </p>
+                            <Embed
+                                id='AXd060dQLW8'
+                                placeholder={pudge}
+                                source='youtube'
+                            />
                         </Grid.Column>
                     </Grid>
                 </Container>
