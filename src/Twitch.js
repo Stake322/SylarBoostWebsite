498a513c2ea0afcd0d2d669e150e288303599a45
Stake322 <81745125+Stake322@users.noreply.github.com>
2021-11-27 03:45:01 +0400

    clean

--- src/Twitch.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const twitch  = () =>{
-    return (
-        <button> Twitch</button>
-    )
-}
-export default twitch;
--- src/Youtube.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Youtube  = () =>{
-    return (
-        <button> АББОБА</button>
-    )
-}
-export default Youtube
